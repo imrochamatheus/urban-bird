b5c512b540cab5b77e8a52bbd623123dc01d3116
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-21 11:31:25 -0300

    feat: add child routes for offer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { OfertaComponent } from './oferta/oferta.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 const routes: Routes = [
   {
@@ -20,11 +22,25 @@ const routes: Routes = [
   },
   {
     path: 'oferta',
-    component: HomeComponent,
+    redirectTo: '',
   },
   {
     path: 'oferta/:id',
     component: OfertaComponent,
+    children: [
+      {
+        path: '',
+        component: ComoUsarComponent,
+      },
+      {
+        path: 'como-usar',
+        component: ComoUsarComponent,
+      },
+      {
+        path: 'onde-fica',
+        component: OndeFicaComponent,
+      },
+    ],
   },
 ];
 
