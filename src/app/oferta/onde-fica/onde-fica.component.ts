9d92b1a2d00070ddb1a15e45f846c9578edebd79
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-21 14:51:20 -0300

    feat: add "onde fica" dynamically

--- src/app/oferta/onde-fica/onde-fica.component.ts
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'src/app/services/ofertas.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-onde-fica',
   templateUrl: './onde-fica.component.html',
-  styleUrls: ['./onde-fica.component.css']
+  styleUrls: ['./onde-fica.component.css'],
 })
 export class OndeFicaComponent implements OnInit {
+  public ondeFica?: string;
 
-  constructor() { }
+  constructor(
+    private ofertasService: OfertasService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    const OfferId = this.route.parent?.snapshot.params['id'];
 
+    this.ofertasService
+      .getOndeFicaById(OfferId)
+      .subscribe((infos) => (this.ondeFica = infos[0].descricao));
+  }
 }
