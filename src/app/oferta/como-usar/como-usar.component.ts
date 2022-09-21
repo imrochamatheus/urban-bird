89e299edcafd1775fee7d3090ca08ac43c0f03b3
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-21 14:51:00 -0300

    feat:  add 'how to use' dynamically

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from 'src/app/services/ofertas.service';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
-  styleUrls: ['./como-usar.component.css']
+  styleUrls: ['./como-usar.component.css'],
 })
 export class ComoUsarComponent implements OnInit {
+  public comoUsar: any;
 
-  constructor() { }
+  constructor(
+    private ofertasService: OfertasService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    const OfferId = this.route.parent?.snapshot.params['id'];
 
+    this.ofertasService
+      .getComoUsarById(OfferId)
+      .subscribe((infos) => (this.comoUsar = infos[0].descricao));
+  }
 }
