00df5bd738d689c94576d3c586c909c75cc974f7
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-21 10:50:50 -0300

    feat: add baseURL property

--- src/app/services/ofertas.service.ts
+++ src/app/services/ofertas.service.ts
@@ -5,19 +5,19 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class OfertasService {
-  private baseUrl = '${this.baseUrl}';
+  private baseUrl = 'http://localhost:3000/ofertas';
 
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Observable<IOfertas[]> {
-    return this.http.get<IOfertas[]>('${this.baseUrl}');
+    return this.http.get<IOfertas[]>(`${this.baseUrl}`);
   }
 
   public getOfertasByCategory(category: string): Observable<IOfertas[]> {
     return this.http.get<IOfertas[]>(`${this.baseUrl}?categoria=${category}`);
   }
 
-  public getOfertasById(id: number): Observable<IOfertas> {
-    return this.http.get<IOfertas>(`${this.baseUrl}?id=${id}`);
+  public getOfertasById(id: number): Observable<IOfertas[]> {
+    return this.http.get<IOfertas[]>(`${this.baseUrl}?id=${id}`);
   }
 }
