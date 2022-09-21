c57205c3ee2f688456f9a44c99c5879f856c664f
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-21 14:52:01 -0300

    feat: Add getComoUsarById && getOndeFicaById methods in the ofertas service

--- src/app/services/ofertas.service.ts
+++ src/app/services/ofertas.service.ts
@@ -5,19 +5,29 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class OfertasService {
-  private baseUrl = 'http://localhost:3000/ofertas';
+  private baseUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Observable<IOfertas[]> {
-    return this.http.get<IOfertas[]>(`${this.baseUrl}`);
+    return this.http.get<IOfertas[]>(`${this.baseUrl}/ofertas`);
   }
 
   public getOfertasByCategory(category: string): Observable<IOfertas[]> {
-    return this.http.get<IOfertas[]>(`${this.baseUrl}?categoria=${category}`);
+    return this.http.get<IOfertas[]>(
+      `${this.baseUrl}/ofertas?categoria=${category}`
+    );
   }
 
   public getOfertasById(id: number): Observable<IOfertas[]> {
-    return this.http.get<IOfertas[]>(`${this.baseUrl}?id=${id}`);
+    return this.http.get<IOfertas[]>(`${this.baseUrl}/ofertas?id=${id}`);
+  }
+
+  public getComoUsarById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/como-usar?id=${id}`);
+  }
+
+  public getOndeFicaById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/onde-fica?id=${id}`);
   }
 }
