7827b202a1cb94609a2cc95436fc3e26b4127bbc
Matheus Rocha <im.rochamatheus@gmail.com>
2022-09-21 10:52:05 -0300

    feat: add offers component

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { IOfertas } from '../services/Ofertas';
 import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from './../services/ofertas.service';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService],
 })
 export class OfertaComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
+
+  public oferta?: IOfertas;
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      const id = params['id'];
-      console.log(params);
-    });
+    const offerId = this.route.snapshot.params['id'];
+
+    this.ofertasService
+      .getOfertasById(offerId)
+      .subscribe((oferta) => (this.oferta = oferta.shift()));
   }
 }
